190a816b52d154dd1cc8f6f7012381c72f63f1ba
karan joshi <karanjoshi6397@gmail.com>
2026-08-07 23:24:01 +0530

    Update Largest Element.cpp

--- Arrays/Largest Element.cpp
+++ Arrays/Largest Element.cpp
@@ -1,1 +1,30 @@
+/*
+Problem: Largest Element in an Array
+Platform: GFG
 
+Approach:
+- Traverse the array once and keep track of the maximum element.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+
+        int n = nums.size();
+        int largest = nums[0];
+
+        for(int i = 1; i < n; i++)
+        {
+            if(nums[i] > largest)
+            {
+                largest = nums[i];
+            }
+        }
+
+        return largest;
+    }
+};
