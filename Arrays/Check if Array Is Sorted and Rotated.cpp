fe92c00898ed1798c4e50d285584cbcf9ba096b5
karan joshi <karanjoshi6397@gmail.com>
2026-09-04 00:03:29 +0530

    Update Check if Array Is Sorted and Rotated.cpp

--- Arrays/Check if Array Is Sorted and Rotated.cpp
+++ Arrays/Check if Array Is Sorted and Rotated.cpp
@@ -1,7 +1,11 @@
 /*
 Given an array nums, determine whether it was originally sorted in non-decreasing order and then rotated some number of times.
 A sorted and rotated array can have at most one point where the current element is greater than the next element, considering the array as circular.
-Example
+
+for checking circuler element like last element to first element we have used modulus operator here 
+there is one property % operator is if the numerator is smller then denominator then it gives same result
+if numrator is equals to denominator then it gives 0 which is the first index of the array.
+
 */
 
 class Solution {
